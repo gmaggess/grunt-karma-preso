c2f74291c81ee86a49aec9a6a5e0e467dc3fca41
George Maggessy <george.maggessy@oracle.com>
2013-10-31 17:23:51 -0700

    grunt file 90% setup

--- README.md
+++ README.md
@@ -1,3 +1,77 @@
 # Grunt and Karma Brown Bag Session
 
+## Grunt Presentation
+__Step 1__. Install Grunt locally
 
+```
+npm install grunt --save-dev
+```
+
+Show that grunt gets added to _package.json_.
+
+__Step 2__. Install the following plugins: 
+
+Open the browser and show where you can look for plugins: http://gruntjs.com/plugins. Show the plugin details such as install command and down the page how to use it.
+
+```
+npm install grunt-contrib-uglify --save-dev
+```
+
+Before configuring uglify, add some basic config:
+
+```javascript
+	var appConfig = {
+		main: 'src/main',
+		test: 'src/test',
+		dist: 'dist',
+		comp: 'bower_components',
+	};
+	...
+        app: appConfig,
+        uglify: {
+          demo: {
+            options: {
+                mangle: false,
+                compress: true,
+                beautify: false,
+                preserveComments: true
+            },
+            files: {
+                '<%= app.dist %>/<%= pkg.name %>.min.js': ['<%= app.main %>/js/*.js']
+            }
+        }
+      }
+   	...
+   	grunt.loadNpmTasks('grunt-contrib-uglify');
+
+    // Default task (includes format checking)
+    grunt.registerTask('default', ['uglify']);
+```
+
+__Step 3__. Configure other plugins:
+
+1. grunt-contrib-jshint
+2. grunt-contrib-clean
+3. grunt-contrib-copy
+4. grunt-contrib-cssmin
+5. grunt-contrib-watch
+
+```
+npm install grunt-contrib-cssmin grunt-contrib-jshint grunt-contrib-watch grunt-contrib-clean grunt-contrib-copy --save-dev
+```
+
+Then add the code below to Grunt:
+
+```
+
+  ...
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-contrib-clean');
+  grunt.loadNpmTasks('grunt-contrib-copy');
+  grunt.loadNpmTasks('grunt-contrib-cssmin');  
+  grunt.loadNpmTasks('grunt-contrib-watch');
+
+  grunt.registerTask('build', ['clean', 'uglify:demo', 'cssmin', 'copy:main']);
+```
+
+Show the grunt plugins on _package.json_ as well. Also show the ``node_modules`` directory content. Mention that you should *not* add node_modules to git, since it will be recreated as soon as someone runs ``npm install`` on the current directory.
--- todomvc/Gruntfile.js
+++ todomvc/Gruntfile.js
@@ -1,7 +1,97 @@
 'use strict';
 
 module.exports = function(grunt) {
+    var appConfig = {
+        main: 'src/main',
+        test: 'src/test',
+        dist: 'dist',
+        comp: 'bower_components',
+    };
+
     grunt.initConfig({
-        pkg: grunt.file.readJSON('package.json')
+        pkg: grunt.file.readJSON('package.json'),
+        app: appConfig,
+        uglify: {
+            demo: {
+                options: {
+                    mangle: false,
+                    compress: true,
+                    preserveComments: true
+                },
+                files: {
+                    '<%= app.dist %>/scripts/<%= pkg.name %>.min.js': ['<%= app.main %>/scripts/*.js']
+                }
+            }
+        },
+        jshint: {
+            all: ['<%= app.main %>/scripts/*.js'],
+            options: {
+                curly: true,
+                eqeqeq: true,
+                eqnull: true,
+                browser: true,
+                globals: {
+                    jQuery: true
+                }
+            }
+        },
+        clean: {
+            build: {
+                src: ['<%= app.dist %>'],
+                options: {
+                    force: true
+                }
+            }
+        },
+        copy: {
+            main: {
+                files: [{
+                    expand: true,
+                    cwd: '<%= app.main %>',
+                    src: [
+                        '*.html'
+                    ],
+                    dest: '<%= app.dist %>'
+                }]
+            }
+        },
+        cssmin: {
+            compress: {
+                files: {
+                    '<%= app.dist %>/resources/css/base.css': ['<%= app.comp %>/todomvc-common/base.css']
+                }
+            }
+        },
+        watch: {
+            options: {
+                livereload: 35729,
+                nospawn: true
+            },
+            html: {
+                files: '<%= app.main %>/*.html',
+                tasks: ['copy:main']
+            },
+            scripts: {
+                files: '<%= app.main %>/scripts/*.js',
+                tasks: ['uglify:demo', 'jshint']
+            },
+            css: {
+                files: '<%= app.dist %>/resources/css/*.css',
+                tasks: ['cssmin']
+            }
+        },
     });
+
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-contrib-jshint');
+    grunt.loadNpmTasks('grunt-contrib-clean');
+    grunt.loadNpmTasks('grunt-contrib-copy');
+    grunt.loadNpmTasks('grunt-contrib-cssmin');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+
+    // Default task (includes format checking)
+    grunt.registerTask('default', ['uglify']);
+    grunt.registerTask('build', ['clean', 'uglify:demo', 'cssmin', 'copy:main']);
+
+
 };
